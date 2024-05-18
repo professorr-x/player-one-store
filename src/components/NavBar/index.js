d6af4e999f0de8f5ef99785d174638f33470165e
Yasser Hussain <hussaiy1@gmail.com>
2024-05-18 22:32:22 +0200

    adding updated mobile nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -30,7 +30,7 @@ const Navbar = () => {
       </a>
 
       {/* Desktop Navigation */}
-      <ul className="hidden md:flex w-1/6">
+      <ul className="flex md:w-1/6 w-full">
         {/* {navItems.map(item => (
           <li
             key={item.id}
@@ -48,34 +48,23 @@ const Navbar = () => {
       </ul>
 
       {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className="block md:hidden">
+      {/* <div onClick={handleNav} className="block md:hidden">
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
+      </div> */}
 
       {/* Mobile Navigation Menu */}
-      <ul
+      {/* <ul
         className={
           nav
             ? "fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
             : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
         }
       >
-        {/* Mobile Logo */}
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
-
-        {/* Mobile Navigation Items */}
-        {/* {navItems.map(item => (
-          <li
-            key={item.id}
-            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
-          >
-            {item.text}
-          </li>
-        ))} */}
         <li className="m-8">
           <WalletMultiButton />
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
